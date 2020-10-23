74d058f856c14ae3a70e2192f20c3ce5e4e7f598
MontejoJorge <jorge.montejo@ikasle.egibide.org>
2020-10-23 14:05:04 +0200

    15-16

--- 01-introduccion/e15.php
+++ 01-introduccion/e15.php
@@ -4,3 +4,19 @@ llamada “getDatos()” que reciba como primer parámetro el nombre de
 usuario y como segundo parámetro el dato a obtener (“nombre”, “apellidos” o
 “email”) y lo muestre por pantalla.
 getDatos($diccionario,“jvadillo”,”email”) → jvadillo@egibide.org -->
+<?php
+$diccionario = array (
+    "Jorge" => array (
+        "apellido" => "Montejo",
+        "email" => "jorge@jorge.com",
+        "telefono" => "666"
+    )
+    );
+
+function getDatos($diccionario,$nombre,$dato) {
+    return $diccionario[$nombre][$dato];
+}
+
+echo getDatos($diccionario,"Jorge","email");
+
+?>
--- 01-introduccion/e16.php
+++ 01-introduccion/e16.php
@@ -2,3 +2,16 @@
 deberá realizar la siguiente operación:
 ● Si los números son distintos, mostrará el resultado de su suma.
 ● Si los números son iguales, mostrará el resultado de su multiplicación. -->
+<?php
+
+$a = 3;
+
+$b = 3;
+
+if ($a!=$b) {
+    echo $a+$b;
+}else {
+    echo $a*$b;
+}
+
+?>
