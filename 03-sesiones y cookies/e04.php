a012d2f5097649ab6d9907847bf55e0a455c4af5
MontejoJorge <jorge.montejo@ikasle.egibide.org>
2020-11-04 11:54:02 +0100

    4

--- 03-sesiones y cookies/e04.php
+++ 03-sesiones y cookies/e04.php
@@ -1,6 +1,31 @@
 <?php
 session_start();
 
-$arr = ["1","2","3"];
+function iniciarArray(){
+    if(!isset($_SESSION["names"])){
+        $_SESSION["names"]=array();
+    }
+    return $_SESSION["names"];
+}
 
-$_SESSION["names"]  = $arr;ñ
+function accion($accion){
+    if ($accion=="insertar"){
+        if (isset($_GET["name"])){
+            $asistente = $_GET["name"];
+            array_push($_SESSION["names"],$asistente);
+        }
+    break;
+    }else {
+        unset($_SESSION["names"]);
+    break;
+    }
+}
+
+if (isset($_GET["accion"])){
+    $accion = $_GET["accion"];
+    accion($accion);
+}
+
+$asistentes = iniciarArray();
+
+require "e04.view.php";
--- 03-sesiones y cookies/e04.view.php
+++ 03-sesiones y cookies/e04.view.php
@@ -8,10 +8,19 @@
 <body>
     <h3>Lista de asistentes</h3>
     <ul>
-
+        <?php
+            if (count($asistentes)>0){
+            foreach ($asistentes as $asistente) {
+            ?>    
+                <li><?=$asistente?></li>
+            <?php
+            }
+        }
+        ?>
     </ul>
     <form action="e04.php" method="get">
         <input type="text" name="name">
+        <input type="hidden" name="accion" value="insertar">
         <input type="submit" value="Añadir">
     </form>
 
